b467d766a3c39210f0bc6c03788f144195b0e94f
Piyush D <piyushdattaca@gmail.com>
2026-02-03 05:08:41 -0800

    fixed up a few imports, everything should be good now

--- metro.config.js
+++ metro.config.js
@@ -5,9 +5,5 @@ const projectRoot = path.resolve(__dirname);
 
 module.exports = async () => {
   const config = await getDefaultConfig(projectRoot);
-  config.server = {
-    ...config.server,
-    entryPoint: "./entry.js",
-  };
   return config;
 };
